44aafbbbe2eb183828f2c9ee0a25d7947d5118c4
David Fekke <david@fekke.com>
2017-05-16 16:47:54 -0400

    Adding in location and a Map marker

--- index.ios.js
+++ index.ios.js
@@ -14,14 +14,49 @@ import {
 import MapView from 'react-native-maps'; 
 
 export default class JNMaps extends Component {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: 30.3203746,
+        longitude: -81.6654225,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      },
+      marker: {
+        title: 'You are here',
+        description: 'This is your current location!'
+      }
+    };
+  }
+  componentWillMount() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const newRegion = {
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            };
+            console.log(`Lat: ${pos.coords.latitude} & Long: ${pos.coords.longitude}`);
+            this.setState(Object.assign({}, { region: newRegion }));
+            if (this.map !== null && this.map !== undefined) {
+                this.map.animateToRegion(newRegion);
+            }
+        });
+    } 
+  }
   render() {
     return (
-        <MapView style={styles.map} initialRegion={{
-          latitude: 30.3203746,
-          longitude: -81.6654225,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }} />
+        <MapView style={styles.map} 
+          ref={ref => { this.map = ref; }} 
+          initialRegion={this.state.region}
+          onRegionChange={this.onRegionChange} >
+          <MapView.Marker.Animated
+              coordinate={this.state.region}
+              title={this.state.marker.title}
+              description={this.state.marker.description} />
+        </MapView>
     );
   }
 }
