5c15a24f6aa9f5179f233c5478ab810255578d15
David Fekke <david@fekke.com>
2017-05-18 17:01:06 -0400

    Adding Android support with Google Maps for Android SDK

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 This is a sample project for React Native Maps for the React Native Jax user group and JaxNode UG.
 
+To run this in Android, you will need to use a simulator that has Google Play Services installed. 
+You will also need to create an API Key for Android Google Maps in the API Console.
+
+You will place that key in the AndroidManifest.xml file in the Application element;
+
+```
+<meta-data
+        android:name="com.google.android.geo.API_KEY"
+        android:value="Your Google maps API Key Here"/>
+```
+
 For Demo used for May 17th 2017.
--- index.android.js
+++ index.android.js
@@ -1,7 +1,8 @@
 /**
- * Sample React Native App
+ * Sample React Native Maps App
  * https://github.com/facebook/react-native
  * @flow
+ * For React Native Jax and JaxNode user groups May 17th 2017
  */
 
 import React, { Component } from 'react';
@@ -11,43 +12,82 @@ import {
   Text,
   View
 } from 'react-native';
+import MapView from 'react-native-maps'; 
 
 export default class JNMaps extends Component {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: 30.3203746,
+        longitude: -81.6654225,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      },
+      marker: {
+        title: 'You are here',
+        description: 'This is your current location!'
+      }
+    };
+  }
+  onRegionChange(region) {
+    this.setState(Object.assign({}, this.state, { region: region }));
+  }
+  componentWillMount() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const newRegion = {
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            };
+            this.setState(Object.assign({}, this.state, { region: newRegion }));
+            fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${pos.coords.latitude},${pos.coords.longitude}`)
+              .then(res => res.json())
+              .then(json => {
+                if (json.results.length > 0) {
+                  this.setState(Object.assign({}, this.state, { marker: { title: 'Your are here', description: json.results[0].formatted_address }}));  
+                }
+              })
+              .catch(err => alert(err));
+            if (this.map !== null && this.map !== undefined) {
+                //this.map.animateToRegion(newRegion);
+            }
+        });
+    }
+    
+  }
+  onRegionChangeComplete(region) {
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${region.latitude},${region.longitude}`)
+      .then(res => res.json())
+      .then(json => {
+        if (json.results.length > 0) {
+          this.setState(Object.assign({}, this.state, { marker: { title: 'Your are here', description: json.results[0].formatted_address }}));  
+        }
+      })
+      .catch(err => alert(err));
+  }
   render() {
+    // onRegionChange={(r) => this.onRegionChange(r)}
+    // onRegionChangeComplete={(r) => this.onRegionChangeComplete(r)} 
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <MapView style={styles.map} 
+          ref={ref => { this.map = ref; }} 
+          initialRegion={this.state.region} >
+          <MapView.Marker.Animated
+              coordinate={this.state.region}
+              title={this.state.marker.title}
+              description={this.state.marker.description} />
+        </MapView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  }
 });
 
 AppRegistry.registerComponent('JNMaps', () => JNMaps);
